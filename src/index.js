4c7bbe254c31e141b610a10243687760040acc43
minhduchcm <minhducle.hcm@gmail.com>
2017-09-18 17:23:57 +0700

    dùng bootstap làm theme chính

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log("Hello world");
+import React from "react";
+import { render } from "react-dom";
+import App from "./components/app";
+import "./bootstrap.scss";
+
+render(<App />, document.getElementById("root"));
